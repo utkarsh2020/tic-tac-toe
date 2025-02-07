0f8929691042e47bb308c9e5c014a50d12fd7fa0
Utkarsh Swarnkar <utkarsh2020@gmail.com>
2025-02-07 10:10:19 +0530

    Added AI player

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-const X_CLASS = 'x'
-const CIRCLE_CLASS = 'circle'
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle';
 const WINNING_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -9,81 +9,106 @@ const WINNING_COMBINATIONS = [
   [2, 5, 8],
   [0, 4, 8],
   [2, 4, 6]
-]
-const cellElements = document.querySelectorAll('[data-cell]')
-const board = document.getElementById('board')
-const winningMessageElement = document.getElementById('winningMessage')
-const restartButton = document.getElementById('restartButton')
-const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
-let circleTurn
+];
 
-startGame()
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+const winningMessageElement = document.getElementById('winningMessage');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+const restartButton = document.getElementById('restartButton');
+const gameModeSelect = document.getElementById('game-mode');
 
-restartButton.addEventListener('click', startGame)
+let circleTurn;
+let isSinglePlayer;
+
+startGame();
+
+restartButton.addEventListener('click', startGame);
+gameModeSelect.addEventListener('change', startGame);
 
 function startGame() {
-  circleTurn = false
+  circleTurn = false;
+  isSinglePlayer = gameModeSelect.value === '1';
   cellElements.forEach(cell => {
-    cell.classList.remove(X_CLASS)
-    cell.classList.remove(CIRCLE_CLASS)
-    cell.removeEventListener('click', handleClick)
-    cell.addEventListener('click', handleClick, { once: true })
-  })
-  setBoardHoverClass()
-  winningMessageElement.classList.remove('show')
+    cell.classList.remove(X_CLASS);
+    cell.classList.remove(CIRCLE_CLASS);
+    cell.removeEventListener('click', handleClick);
+    cell.addEventListener('click', handleClick, { once: true });
+  });
+  setBoardHoverClass();
+  winningMessageElement.classList.remove('show');
 }
 
 function handleClick(e) {
-  const cell = e.target
-  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
-  placeMark(cell, currentClass)
+  const cell = e.target;
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+  placeMark(cell, currentClass);
   if (checkWin(currentClass)) {
-    endGame(false)
+    endGame(false);
   } else if (isDraw()) {
-    endGame(true)
+    endGame(true);
   } else {
-    swapTurns()
-    setBoardHoverClass()
+    swapTurns();
+    if (isSinglePlayer && !circleTurn) {
+      makeAIMove();
+    }
+    setBoardHoverClass();
   }
 }
 
 function endGame(draw) {
   if (draw) {
-    winningMessageTextElement.innerText = 'Draw!'
+    winningMessageTextElement.innerText = 'Draw!';
   } else {
-    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`;
   }
-  winningMessageElement.classList.add('show')
+  winningMessageElement.classList.add('show');
 }
 
 function isDraw() {
   return [...cellElements].every(cell => {
-    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
-  })
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS);
+  });
 }
 
 function placeMark(cell, currentClass) {
-  cell.classList.add(currentClass)
+  cell.classList.add(currentClass);
 }
 
 function swapTurns() {
-  circleTurn = !circleTurn
+  circleTurn = !circleTurn;
 }
 
 function setBoardHoverClass() {
-  board.classList.remove(X_CLASS)
-  board.classList.remove(CIRCLE_CLASS)
+  board.classList.remove(X_CLASS);
+  board.classList.remove(CIRCLE_CLASS);
   if (circleTurn) {
-    board.classList.add(CIRCLE_CLASS)
+    board.classList.add(CIRCLE_CLASS);
   } else {
-    board.classList.add(X_CLASS)
+    board.classList.add(X_CLASS);
   }
 }
 
 function checkWin(currentClass) {
   return WINNING_COMBINATIONS.some(combination => {
     return combination.every(index => {
-      return cellElements[index].classList.contains(currentClass)
-    })
-  })
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
+}
+
+function makeAIMove() {
+  const availableCells = [...cellElements].filter(cell => {
+    return !cell.classList.contains(X_CLASS) && !cell.classList.contains(CIRCLE_CLASS);
+  });
+  const randomCell = availableCells[Math.floor(Math.random() * availableCells.length)];
+  placeMark(randomCell, X_CLASS);
+  if (checkWin(X_CLASS)) {
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  } else {
+    swapTurns();
+    setBoardHoverClass();
+  }
 }
